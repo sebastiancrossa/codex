e728570055d29d555e21c23563e7f4d991e7f09b
Sebastian C <crossasebastian@gmail.com>
2018-05-27 15:14:49 -0600

    Initial Commit

---
+++ codeX/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  codeX
+//
+//  Created by Sebastian Crossa on 5/27/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
