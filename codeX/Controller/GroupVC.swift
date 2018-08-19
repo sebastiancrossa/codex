da651ff9354f7fd0a6b5a644d33b96a11cd73f4b
Sebastian C <crossasebastian@gmail.com>
2018-08-19 12:34:15 -0500

    Groups vc now fetches the correct groups form backend

--- codeX/Controller/GroupVC.swift
+++ codeX/Controller/GroupVC.swift
@@ -12,11 +12,27 @@ class GroupVC: UIViewController {
 
     @IBOutlet weak var groupsTableView: UITableView!
     
+    var groupsArray = [Group]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         groupsTableView.delegate = self
         groupsTableView.dataSource = self
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                
+                self.groupsTableView.reloadData()
+            }
+        }
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -31,13 +47,15 @@ extension GroupVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
         
-        cell.configureCell(title: "Admin Chat", description: "Board of directors only", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        
+        cell.configureCell(title: group.groupTitle, description: group.groupDescription, memberCount: group.memberCount)
         
         return cell
     }
